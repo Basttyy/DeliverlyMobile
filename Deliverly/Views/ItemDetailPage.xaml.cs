df18f5ecd850cbf4d8530cfe175cdd83d78571ff
Abdulbasit Mamman <basttyy@gmail.com>
2020-08-28 11:24:04 +0100

    Add project files.

---
+++ Deliverly/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using Deliverly.ViewModels;
+
+namespace Deliverly.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
